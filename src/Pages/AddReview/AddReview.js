9d09919001388d1c3114d4196dabee6f25a524d0
Sharif-Billah <sharifbillah1998@gmail.com>
2022-02-15 02:27:10 +0600

    fublic

--- src/Pages/AddReview/AddReview.js
+++ src/Pages/AddReview/AddReview.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AddReview = () => {
     return (
         <div>
-            <h3>This is Review Pages</h3>
+            <h2>Add a review</h2>
         </div>
     );
 };
